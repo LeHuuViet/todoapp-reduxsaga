0e8a8aed7557ac8b9df3bf1726b0018253802587
Le Huu Viet <lhv.nk53@gmail.com>
2022-08-19 15:56:05 +0700

    fourth commit

--- src/Components/FetchTodoList.js
+++ src/Components/FetchTodoList.js
@@ -1,26 +1,28 @@
-import { Button } from "antd";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { loadTodoStart } from "../redux/app.action";
 import TodoItem from "./TodoItem";
 
 const FetchTodo = () => {
+  const [loading, setLoading] = useState(false);
   const dispatch = useDispatch();
-  const [loading,setLoading]=useState(false)
-  const fetchTodo = () => {
-    dispatch(loadTodoStart());
-  };
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+      dispatch(loadTodoStart());
+    }, 1000);
+  }, [dispatch]);
+
   const state = useSelector((state) => state.app);
-  if(loading) return <h1>Loading....</h1>
+  if (loading) return <h1>Loading...</h1>;
   return (
     <>
       <h1 style={{ textAlign: "left" }}>Todo App using ReduxSaga</h1>
       <div style={{ textAlign: "left" }}>
-        {!state.loading &&
-          state.todos.map((todo) => <TodoItem key={todo.id} todo={todo}  />)}
-        <Button type="primary" onClick={fetchTodo}>
-          Fetch Todo
-        </Button>
+        {state.todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
       </div>
     </>
   );
